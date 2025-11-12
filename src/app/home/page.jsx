f05bf1297e8002bb011887afa107b22b9421fe0b
oldiney <oldineygalvao@gmail.com>
2025-11-12 19:39:09 -0300

    feat: pagina sugestoes criada e componente slider estruturado

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,11 +1,24 @@
 import Navbar from "@/components/navbar"
+import Image from "next/image";
+import styles from "./page.module.css"
+import Slider from "@/components/slider";
 
 export default function Home(){
     return(
         
         <div>
             <Navbar></Navbar>
-            <p>Home works</p>
+            
+           <div className={styles.telaInicial}> 
+                <Image
+                        src='/logo-inicial.svg'
+                        width={400}
+                        height={400}
+                        alt="imagem da logo(um mascote 'pipoca') junto a uma mensagem de Boas-Vindas e o logo da Triflix. "
+                />
+           </div>
+
+           <Slider></Slider>
         </div>
     );
 }
--- src/app/suggestions/page.jsx
+++ src/app/suggestions/page.jsx
@@ -1,11 +1,40 @@
 import Navbar from "@/components/navbar";
+import styles from "./page.module.css"
+import Slider from "@/components/slider";
 
 export default function Suggestions(){
     return(
         <div>
             <Navbar></Navbar>
 
-            Suggestions Works!
+            <div className={styles.telaInicial}> 
+                <div>
+                    <h1>Você pode gostar de:</h1>
+                    <br />
+                    <div className={styles.containerLogo}>
+
+                    </div>
+                </div>
+
+                <div>
+                    <br />
+                    <br />
+                    <div className={styles.containerTexto}>
+                        <div className={styles.conteudo}>
+                            <h1>Título</h1>
+                            <br />
+                            <br />
+                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Atque nisi, debitis accusantium reiciendis beatae itaque ab repellat enim expedita laboriosam a doloremque illum autem quo laudantium nam, nostrum ad deleniti.
+                            Voluptas ratione eius, quasi commodi voluptatem officia explicabo provident accusantium fugit eum maiores? Nostrum laudantium architecto, recusandae necessitatibus aliquid, nisi tempore debitis expedita blanditiis fugiat ex? Culpa, hic fuga! Aliquid.
+                            Sunt et sapiente soluta. Rem dolor velit inventore, molestiae nisi voluptates maxime magni placeat molestias quidem aspernatur deserunt voluptatum labore illo itaque nam dolorum et obcaecati? Voluptatem sequi aliquam beatae.
+                            Blanditiis delectus deleniti ipsa nostrum sunt sint harum. Dolorum provident voluptates pariatur iste quos perferendis similique laboriosam sunt, quisquam odio aliquid, impedit, velit et? Magnam esse neque minima sed non?
+                            Vero explicabo nisi neque tenetur iusto, dolore tempora nulla consectetur repellendus cum magni ab sed? Esse, reiciendis eligendi illo quos adipisci id libero veritatis eaque veniam voluptates architecto nam natus!</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <Slider></Slider>
         </div>
     );
 }
